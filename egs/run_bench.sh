c5e13427bad0e53813d1cedb9f05f3c39c3beb0d
Aaditya Naik <aadi.naik@gmail.com>
2021-03-04 12:35:28 -0500

    Fixed bugs, renamed unrealizable benchmarks

--- egs/run_bench.sh
+++ egs/run_bench.sh
@@ -6,5 +6,5 @@ for i in $(find ${bench_dir}/* -type f -name "rules.small.dl")
 do
         ./run_single.sh "$(dirname $i)" > /dev/null
 	n=$((n+1))
-	echo -ne "Run for $n / $t \r"
+	# echo -ne "Run for $n / $t \r"
 done
--- ilasp/run_bench.sh
+++ ilasp/run_bench.sh
@@ -5,5 +5,5 @@ for i in $(find ${bench_dir}/* -type f -name "ilasp_bench.las")
 do
 	d=$(dirname $i)
 	echo $d
-	./run_single $d
+	./run_single.sh $d
 done
