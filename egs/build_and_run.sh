44d1b399bb82685213fd75e5be5a930b99ebe5a8
Aaditya Naik <aadi.naik@gmail.com>
2021-03-04 14:13:27 -0500

    Added build_and_run script for EGS

---
+++ egs/build_and_run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+sbt assembly
+
+export LOGNAME=`date +%s | sed 's/^/r/'`
+echo "Echoing output in $LOGNAME.out and piping log messages to $LOGNAME.log"
+
+# Change heap space limits with
+# export JAVA_OPTS="-Xms16g -Xmx24g"
+
+# Print GC statistics with
+# export JAVA_OPTS="-XX:-PrintGC -XX:-PrintGCDetails"
+# https://stackoverflow.com/a/466919
+
+# Profile using VisualVM
+
+scala ./target/scala-2.13/egs-assembly-0.1.0-SNAPSHOT.jar "$@" 2> $LOGNAME.log | tee $LOGNAME.out
+echo $?
