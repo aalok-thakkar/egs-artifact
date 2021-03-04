fc54043e05d4a26de0399f88c3253f92c13d37b9
Aaditya Naik <aadi.naik@gmail.com>
2021-03-04 03:25:50 -0500

    Fixed some setup bugs

--- scythe/run_single.sh
+++ scythe/run_single.sh
@@ -4,5 +4,5 @@ bench="$1"
 echo "Running Scythe on $bench"
 mkdir -p logs/${bench}
 rm -rf logs/${bench}/*
-timeout 5m time java -jar out/artifacts/Scythe_jar/Scythe.jar ${bench} StagedEnumerator > logs/${bench}/ScytheOP.out
+timeout 5m java -jar out/artifacts/Scythe_jar/Scythe.jar ${bench} StagedEnumerator > logs/${bench}/ScytheOP.out
 echo "Scythe finished running on $bench"
